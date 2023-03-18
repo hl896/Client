f05b5fa6990812936a18a3a7cf68f3599f8636d7
hl896x <yeung516001@gmail.com>
2023-03-18 13:47:10 +0000

    fix access token

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 const BASE_URL = "http://hyweb.hopto.org:5000/api/";
+const user =JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const token = currentUser?.accessToken;
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzY2NlZjBlMzExZTE2NzBlM2JiYWM5NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3ODYwMjU3OSwiZXhwIjoxNjc4ODYxNzc5fQ.CZVx0D8cUX4CjESl_7OJbk0IxPn7enMwOgUN9d0h6t8"
+// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzY2NlZjBlMzExZTE2NzBlM2JiYWM5NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3ODYwMjU3OSwiZXhwIjoxNjc4ODYxNzc5fQ.CZVx0D8cUX4CjESl_7OJbk0IxPn7enMwOgUN9d0h6t8"
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
 
